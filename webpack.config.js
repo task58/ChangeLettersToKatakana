156122816dcabffdf36c500074a9e6d91b49b119
task58 <taskr3g@gmail.com>
2023-09-02 09:03:07 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+ 
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/index.html'
+    })
+  ]
+};
